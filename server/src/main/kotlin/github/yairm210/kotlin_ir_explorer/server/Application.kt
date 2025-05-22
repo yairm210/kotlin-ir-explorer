fb2879e2dcd904ba3b1cc0b12b7f0e078aea2354
Yair Morgenstern <yairm210@hotmail.com>
2025-05-22 23:37:49 +0300

    Add ktor server that can get code and return mermaid graph

---
+++ server/src/main/kotlin/github/yairm210/kotlin_ir_explorer/server/Application.kt
@@ -0,0 +1,70 @@
+package github.yairm210.kotlin_ir_explorer.server
+
+import getCompilationResult
+import github.yairm210.kotlin_ir_explorer.core.IrMermaidGraphConverter
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import org.jetbrains.kotlin.ir.util.dump
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+
+
+    install(ContentNegotiation) {
+        json()
+    }
+}
+
+
+fun Application.configureRouting() {
+    routing {
+        get("/api/kotlinToMermaid/{kotlinCode}") {
+            // get 'kotlin parameter
+            val kotlinCode = call.parameters["kotlinCode"] ?: return@get call.respondText(
+                "Missing or malformed 'kotlin' parameter",
+                status = HttpStatusCode.BadRequest
+            )
+
+            val compilationResult = getCompilationResult(kotlinCode)
+            
+            @Serializable
+            class Response(
+                val mermaidGraph: String?,
+                val messages: List<String>
+            )
+            
+            val mermaidGraph = if (compilationResult.irModuleFragment == null) null
+                    else IrMermaidGraphConverter.convertToMermaidGraph(compilationResult.irModuleFragment!!)
+            
+            if (compilationResult.irModuleFragment == null) println("Result is null")
+            else {
+                compilationResult.irModuleFragment!!.files
+                    .forEach { file ->
+                        file.declarations.forEach { declaration ->
+                            println(declaration.dump())
+                        }
+                    }
+            }
+
+            val renderedMessages = compilationResult.messages.map { "${it.severity}: ${it.message} ${it.location?.let { "${it.line}:${it.column}" }}" }
+            
+            val response = Response(
+                mermaidGraph,
+                renderedMessages
+            )
+            call.respond(response)
+        }
+        // Static plugin. Try to access `/static/index.html`
+        staticResources("/", "static")
+    }
+}
