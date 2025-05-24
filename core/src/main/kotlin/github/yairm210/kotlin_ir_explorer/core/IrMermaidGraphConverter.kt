c048c46baa3cfeda97d65c897ee09a450f0e8ec1
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-05-25 00:54:15 +0300

    Mermaid: Wrap functions in subgraphs with the function name

--- core/src/main/kotlin/github/yairm210/kotlin_ir_explorer/core/IrMermaidGraphConverter.kt
+++ core/src/main/kotlin/github/yairm210/kotlin_ir_explorer/core/IrMermaidGraphConverter.kt
@@ -1,6 +1,7 @@
 package github.yairm210.kotlin_ir_explorer.core
 
 import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
 import org.jetbrains.kotlin.ir.util.render
@@ -12,11 +13,7 @@ object IrMermaidGraphConverter {
         val sb = StringBuilder()
         sb.appendLine("graph TD")
         irModuleFragment.files.forEach { file ->
-            file.declarations.forEach { declaration ->
-                sb.append("subgraph ${declaration.startOffset}-${declaration.endOffset}\n")
-                declaration.accept(IrMermaidGraphListener(), Triple(null, sb, 0))
-                sb.append("end")
-            }
+            file.accept(IrMermaidGraphListener(), Triple(null, sb, 0))
         }
         return sb.toString()
     }
@@ -29,6 +26,11 @@ class IrMermaidGraphListener:IrElementVisitor<Unit, Triple<IrElement?, StringBui
         // Don't use stringbuilder.repeat since it's new to JVM 21
         stringbuilder.append("  ".repeat(depth))
         // Need to escape the rendered string to avoid issues with mermaid
+
+        if (element is IrFunction) {
+            stringbuilder.appendLine("subgraph ${element.name}")
+        }
+        
         stringbuilder.appendLine("${element.hashCode()}[\"${element.render()}\"]")
         
         // IDs of the elements are the hashcodes
@@ -38,5 +40,10 @@ class IrMermaidGraphListener:IrElementVisitor<Unit, Triple<IrElement?, StringBui
         }
         // User-visible text is just the classname for now
         element.acceptChildren(this, Triple(element, stringbuilder, depth + 1))
+
+
+        if (element is IrFunction) {
+            stringbuilder.appendLine("end")
+        }
     }
 }
